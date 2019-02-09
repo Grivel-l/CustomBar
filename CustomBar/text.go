25e3577b42b97de019bda6412678bf6287bd6dae
Léo <legrivel@student.le-101.fr>
2019-02-09 02:27:08 +0100

    Catching error from Text func

--- CustomBar/text.go
+++ CustomBar/text.go
@@ -4,13 +4,18 @@ import (
     "image/color"
 )
 
-func printString(window Window, content string, pos Pos) {
-    window.img.Text(pos.x, pos.y, color.RGBA{
+func printString(window Window, content string, pos Pos) (error) {
+    var err error
+
+    _, _, err = window.img.Text(pos.x, pos.y, color.RGBA{
         R: 0xff,
         G: 0xff,
         B: 0xff,
         A: 0xff,
     }, 16, window.font, content)
+    if (err != nil) {
+        return err
+    }
     window.img.XDraw()
     window.img.XPaint(window.win.Id)
 }
