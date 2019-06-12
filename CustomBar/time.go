6d74a6e3d024cafb8614c430c139e52f404a491d
Léo <legrivel@student.le-101.fr>
2019-06-12 17:38:51 +0200

    Made the time module clickable to print seconds

--- CustomBar/time.go
+++ CustomBar/time.go
@@ -9,16 +9,31 @@ import (
 )
 
 func initDate(signals *Signals) {
+    var accurate    bool
+    var filter      *core.QObject
+    var timer       *time.Timer
+
+    accurate = false
+    filter = core.NewQObject(nil)
+    filter.ConnectEventFilter(func (watched *core.QObject, event *core.QEvent) bool {
+        if (event.Type() == core.QEvent__MouseButtonPress) {
+            accurate = !accurate
+            timer.Stop()
+            printDate(signals, &accurate, &timer)
+        }
+        return false
+    })
     texts["time"] = widgets.NewQLabel(nil, 0)
     texts["time"].SetAlignment(core.Qt__AlignCenter)
     texts["time"].SetStyleSheet("color: white")
-    printDate(signals)
+    texts["time"].InstallEventFilter(filter)
+    printDate(signals, &accurate, &timer)
 }
 
-func printDate(signals *Signals) {
+func printDate(signals *Signals, accurate *bool, timer **time.Timer) {
     var tmp         int
     var timestamp   time.Time
-    var parsed      [5]byte
+    var parsed      [8]byte
     var builder     strings.Builder
 
     timestamp = time.Now()
@@ -29,8 +44,20 @@ func printDate(signals *Signals) {
     tmp = timestamp.Minute()
     parsed[3] = byte(tmp / 10 + 48)
     parsed[4] = byte(tmp % 10 + 48)
-    fmt.Fprintf(&builder, "%v %v %v, %v", timestamp.Weekday().String()[:3], timestamp.Day(), timestamp.Month().String(), string(parsed[:]))
+    if (*accurate) {
+        parsed[5] = ':'
+        tmp = timestamp.Second()
+        parsed[6] = byte(tmp / 10  + 48)
+        parsed[7] = byte(tmp % 10  + 48)
+        fmt.Fprintf(&builder, "%v %v %v, %v", timestamp.Weekday().String()[:3], timestamp.Day(), timestamp.Month().String(), string(parsed[:]))
+    } else {
+        fmt.Fprintf(&builder, "%v %v %v, %v", timestamp.Weekday().String()[:3], timestamp.Day(), timestamp.Month().String(), string(parsed[:5]))
+    }
     signals.UpdateWidget("time", builder.String())
     tmp = timestamp.Second()
-    time.AfterFunc(time.Duration((60 - tmp) * 1000000000), func() {printDate(signals)})
+    if (*accurate) {
+        *timer = time.AfterFunc(time.Duration(1000000000), func() {printDate(signals, accurate, timer)})
+    } else {
+        *timer = time.AfterFunc(time.Duration((60 - tmp) * 1000000000), func() {printDate(signals, accurate, timer)})
+    }
 }
